8173404eab179cca904d4ff6be5c66924ff389c2
Djuradj Perunovic <djuradj.perunovic@bild-studio.net>
2024-08-09 21:00:04 +0200

    Initial Commit

---
+++ iToolkit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iToolkit
+//
+//  Created by Djuradj Perunovic on 9.8.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iToolkit/iToolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iToolkitApp.swift
+//  iToolkit
+//
+//  Created by Djuradj Perunovic on 9.8.24..
+//
+
+import SwiftUI
+
+@main
+struct iToolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
